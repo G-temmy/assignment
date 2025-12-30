f747468d22de8f5ef4c4ac82d2a5bfe1ed77fc4a
GLO-BABE <gloriaomofoye16@gmail.com>
2025-12-30 15:22:28 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+const closeMenu = () => {
+  hamburger.classList.remove("active");
+  navLinks.classList.remove("active");
+};
+
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+window.addEventListener("scroll", () => {
+  if (navLinks.classList.contains("active")) {
+    closeMenu();
+  }
+});
+
+const typed = new Typed(".multiple", {
+  strings: ["Frontend Developer", "Web Designer", "Problem Solver"],
+  typeSpeed: 80,
+  backSpeed: 60,
+  backDelay: 1500,
+  loop: true,
+});
